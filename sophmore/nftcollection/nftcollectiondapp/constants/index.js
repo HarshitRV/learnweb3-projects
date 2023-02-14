9b584d77103f6f1910d6efb49119e3fb78df65e8
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2023-02-14 09:51:06 +0530

    add crptodev frontend for nft sale

---
+++ sophmore/nftcollection/nftcollectiondapp/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xA1f8FdFC0838b13f4B8AFf040edeeF07A1FAd307";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://white-listdapp.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
